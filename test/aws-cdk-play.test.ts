7b8a6132fb257ed395a6b7475f7e4ac4f170ac96
IsiomaC <isioma.chuck@gmail.com>
2023-04-24 19:52:07 +0100

    Initial commit

---
+++ test/aws-cdk-play.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as AwsCdkPlay from '../lib/aws-cdk-play-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/aws-cdk-play-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new AwsCdkPlay.AwsCdkPlayStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
